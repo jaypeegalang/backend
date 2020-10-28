212856382b35937f7e7cd981358074097205b76b
unknown <you@example.com>
2020-10-28 10:11:50 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // 
+    public function registration(Request $request){
+
+        $this->validate($request, [
+            'name' => 'required|string|max:255|min:3',
+            'email' => 'required|email|unique:users',
+            'mobile_number' => 'required|integer|max:10|min:10',
+            'password' => 'required|min:8|confirmed',
+            
+        ]);
+
+        $user = User::firstOrNew(['id' => $request->input("id")]);
+        $request = $request->all();
+        $user = extract_field_to_save($user,$request);
+
+        $user->save();
+
+        return $user->id;
+    }
+
+    
+}
